9239f7450b9b03c266eae3314c6d34872006db71
Matt Despuig <134629447+mdespuig@users.noreply.github.com>
2025-04-28 08:39:16 +0800

    Update widget_test.dart

--- Quadrio_WhoLuh/test/widget_test.dart
+++ Quadrio_WhoLuh/test/widget_test.dart
@@ -2,15 +2,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wholuh/registrationPage.dart';
 import 'package:wholuh/login.dart';
 import 'package:wholuh/homepage.dart';
-import 'package:wholuh/DifficultyPage.dart';
 import 'package:wholuh/gamemodeSelection.dart';
+import 'package:wholuh/vanillaDifficulty.dart';
+import 'package:wholuh/silhouetteDifficulty.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(RegistrationApp());
     await tester.pumpWidget(Login());
     await tester.pumpWidget(home_page());
-    await tester.pumpWidget(Difficulty());
+    await tester.pumpWidget(vanillaDifficulty());
+    await tester.pumpWidget(silhouetteDifficulty());
     await tester.pumpWidget(gamemodePage());
   });
 }
